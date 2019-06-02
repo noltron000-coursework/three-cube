cb2acc6c8a8d1325e7f152573deea4f1ba6585ba
Nolan Kovacik <noltron000@gmail.com>
2019-06-02 01:31:35 -0700

    adding camera

--- main.js
+++ main.js
@@ -3,6 +3,46 @@ const width = window.innerWidth
 const height = window.innerHeight
 
 
+// CREATE INITIAL THREE SCENE
+const scene = new THREE.Scene
+
+
+// CREATE CUBE
+// set up geometry and texture/color
+const cubeGeometry = new THREE.BoxGeometry(100, 100, 100)
+const cubeMaterial = new THREE.MeshBasicMaterial({
+	color: 0x1ec876
+})
+// generate cube geometry
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+// rotate cube by 45 degrees, converted to radians
+cube.rotation.y = Math.PI * 45 / 180
+// finally, add it to our scene
+scene.add(cube)
+
+
+// CREATE CAMERA
+// field of view determines how flat or deep an image is.
+const camFOV = 45
+// cameras are rectangular in THREE.
+// they can be wide or tall, or exactly square.
+// these properties are a rectangle's aspect ratio.
+const camRatio = width / height
+// the min distance at which an item will be rendered.
+const camRenderMin = 0.1
+// the max distance at which an item will be rendered.
+const camRenderMax = 100000
+// finally, we can put these declarations to good use.
+const camera = new THREE.PerspectiveCamera(
+	camFOV, camRatio, camRenderMin, camRenderMax
+)
+camera.position.y = 160
+camera.position.z = 400
+camera.lookAt(cube.position)
+// add the new camera to the scene.
+scene.add(camera)
+
+
 // CREATE RENDERER CANVAS
 // antialias smooths pixelated edges
 const renderer = new THREE.WebGLRenderer({
@@ -12,21 +52,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(width, height)
 // append the renderer canvas to the DOM
 document.body.appendChild(renderer.domElement)
-
-
-// CREATE SCENE
-const scene = new THREE.Scene
-
-
-// CREATE CUBE
-// set up geometry and texture/color
-const cubeGeometry = new THREE.CubeGeometry(100, 100, 100)
-const cubeMaterial = new THREE.MeshLambertMaterial({
-	color: 0x1ec876
-})
-// generate cube geometry
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-// rotate cube by 45 degrees, converted to radians
-cube.rotation.y = Math.PI * 45 / 180
-// finally, add it to our scene
-scene.add(cube)
+// finally, render the scene from the camera's perspective.
+// without the camera, we would be blind.
+// without the scene, the camera has nothing to look at.
+renderer.render(scene, camera)
