2a30edd3df8bc423a0bbe6ca97dad76f5b588b41
Nolan Kovacik <noltron000@gmail.com>
2019-06-02 01:14:07 -0700

    creating cube

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const width = window.innerWidth
 const height = window.innerHeight
 
+
 // CREATE RENDERER CANVAS
 // antialias smooths pixelated edges
 const renderer = new THREE.WebGLRenderer({
@@ -12,5 +13,20 @@ renderer.setSize(width, height)
 // append the renderer canvas to the DOM
 document.body.appendChild(renderer.domElement)
 
+
 // CREATE SCENE
 const scene = new THREE.Scene
+
+
+// CREATE CUBE
+// set up geometry and texture/color
+const cubeGeometry = new THREE.CubeGeometry(100, 100, 100)
+const cubeMaterial = new THREE.MeshLambertMaterial({
+	color: 0x1ec876
+})
+// generate cube geometry
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+// rotate cube by 45 degrees, converted to radians
+cube.rotation.y = Math.PI * 45 / 180
+// finally, add it to our scene
+scene.add(cube)
